105cf6d8c5b2251914f20baea8d07e28cf33eb93
SouCraK12345 <181223239+SouCraK12345@users.noreply.github.com>
2026-04-04 21:05:22 +0900

    リザルトにスコアを表示

--- src/Result.js
+++ src/Result.js
@@ -19,7 +19,8 @@ function Result() {
         resultHTML = (
             <div className="result">
                 総問題数: {data.total}<br />
-                単語数: {data.num_words}
+                単語数: {data.num_words}<br />
+                スコア: {data.btb_total}<br />
             </div>
         )
     }
